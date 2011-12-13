f38a3cd5766dd886c79d1a0c9a2bb0ad1031b494
Jason Smith (work) <jhs@iriscouch.com>
2011-12-13 10:33:21 +0000

    Building out the API

--- test/builder.js
+++ test/builder.js
@@ -4,7 +4,23 @@ var tap = require('tap')
 var api = require('../api')
 
 test('Builder API', function(t) {
-  t.doesNotThrow(function() { return new api.Builder }, 'Create a new builder')
+  var builder;
+
+  t.doesNotThrow(function() { builder = new api.Builder }, 'Create a new builder')
+  t.ok(builder, 'Got the builder object')
+
+  var did = {}
+  builder.on('template', function(template) { did.template = template })
+  builder.on('output'  , function(output)   { did.output   = output   })
+  builder.on('couch'   , function(couch)    { did.couch    = couch    })
+
+  builder.template = null
+  t.throws(function() { builder.start() }, 'Throw for missing template')
+  t.notOk(did.template, 'Template not fired when missing')
+
+  builder.template = 1;
+  t.throws(function() { builder.start() }, 'Throw for bad template type')
+  t.notOk(did.template, 'Template not fired when bad type')
 
   t.end()
 })
