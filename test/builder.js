d3774cffbd44bbb4dbf3b2e843455783de491d87
Jason Smith (work) <jhs@iriscouch.com>
2011-12-13 11:36:25 +0000

    The prep work

--- test/builder.js
+++ test/builder.js
@@ -1,7 +1,11 @@
 var tap = require('tap')
   , test = tap.test
+  , request = require('request')
 
 var api = require('../api')
+
+var DB = 'http://localhost:5984/test_static_plus'
+
 
 test('Builder API', function(t) {
   var builder;
@@ -14,33 +18,51 @@ test('Builder API', function(t) {
   var did = {}
   builder.on('template', function(template) { did.template = template })
   builder.on('output'  , function(output)   { did.output   = output   })
-  builder.on('couch'   , function(couch)    { did.couch    = couch    })
+  builder.on('source'  , function(source)   { did.source   = source    })
   builder.on('start'   , function()         { did.start    = true     })
 
   builder.template = null
   t.throws(start, 'Throw for missing template')
-  t.notOk(did.template, 'Template not fired when missing')
 
   builder.template = 1;
   t.throws(start, 'Throw for bad template type')
-  t.notOk(did.template, 'Template not fired when bad type')
-
   builder.template = function(doc) { return 'Doc!' }
 
   t.throws(start, 'Throw for missing output')
-  t.ok(did.template, 'The template finally fired')
-  t.notOk(did.output, 'But the output event still has not fired')
-
   builder.output = __dirname + '/output'
-  t.throws(start, 'Throw for missing couch')
-//  t.ok(did.output, 'But the output event fired')
 
-  t.notOk(did.start, 'Did not start yet, after good template')
+  t.throws(start, 'Throw for missing source db')
 
-  setTimeout(check_events, 100)
+  builder.source = 'not_a_url'
+  t.throws(start, 'Throw for bad Couch URL')
+
+  var http_begin = new Date;
+  request({method:'DELETE', json:true, uri:DB}, function(er, res) {
+    if(er) throw er;
+    if(res.error && res.error != 'not_found')
+      throw new Error('Failed to delete test DB: ' + JSON.stringify(res.body))
+
+    request({method:'PUT', json:true, uri:DB}, function(er, res) {
+      if(er) throw er;
+      if(res.error && res.error != 'file_exists')
+        throw new Error('Failed to create test DB: ' + JSON.stringify(res.body))
+
+      var http_end = new Date
+        , http_duration = http_end - http_begin
+
+      builder.source = DB
+      t.doesNotThrow(start, 'No throw for all good starting data')
+
+      setTimeout(check_events, http_duration * 1.5)
+    })
+  })
 
   function check_events() {
     t.ok(did.output, 'The output event finally fired')
+    t.ok(did.template, 'The template event finally fired')
+    t.ok(did.source, 'The source event finally fired')
+    t.ok(did.start, 'The builder started')
+
     t.end()
   }
 })
